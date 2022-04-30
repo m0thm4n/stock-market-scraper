cbf1b74e6926654115c9a0d966068e7338a05fc0
Timmy <timmy@pop-os.localdomain>
2022-04-30 10:34:12 -0400

    Deleted db.rs and modified get_tickers.rs to write to the database after scraping the symbols.

--- src/get_tickers.rs
+++ src/get_tickers.rs
@@ -4,7 +4,13 @@ use tokio;
 use std::fs::File;
 use std::io::prelude::*;
 use scraper::{Html, Selector};
-use crate::db::add_symbol_to_db;
+use tokio_postgres::Error;
+use tokio_postgres::tls::{NoTls};
+
+struct Stock {
+    id: i32,
+    symbol: String
+}
 
 // https://stockanalysis.com/stocks/
 // https://eoddata.com/stocklist/NYSE/
@@ -32,16 +38,50 @@ pub async fn get_ticker_names() -> Result<(), Box<dyn std::error::Error>> {
         for title in document.select(&selector) {
             let url = title.value().attr("href").expect("href not found").to_string();
             if url != "/" || url != "." || url != ".." {
-                urls.insert(url.replace("/equities/", ""));
+                urls.insert(url.replace("/equities/", "").to_string());
             }
 
         }
 
         for url in urls{
-            add_symbol_to_db(url.replace("/equities/", ""));
+            add_symbol_to_db(url).await;
+            // println!("Added url {} to db", url)
         }
 
     }
+
+    Ok(())
+}
+
+async fn add_symbol_to_db(symbol: String) -> Result<(), Error> {
+// Connect to the database.
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres password=Babycakes15! dbname=stocks port=5432 sslmode=disable", NoTls).await?;
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    client.query("CREATE TABLE stock_symbol (
+        id SERIAL PRIMARY KEY,
+        symbol VARCHAR NOT NULL
+    )", &[]).await;
+
+    println!("Created DB Table");
+
+    let stock = Stock {
+        id: 0,
+        symbol,
+    };
+
+    client.query(
+        "INSERT INTO stock_symbol (symbol) VALUES ($1)",
+        &[&stock.symbol],
+    ).await;
 
     Ok(())
 }
